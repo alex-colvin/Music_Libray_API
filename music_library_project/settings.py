8d69f76a9c3a1e79202d6d6c1944b0228a5cf91a
alex-colvin <alexcolvin@live.com>
2022-05-06 12:43:36 -0400

    added installed apps in settings.py + updated urls + registered app and admin site in admin.py + created songserializer

--- music_library_project/settings.py
+++ music_library_project/settings.py
@@ -35,7 +35,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
+    'rest_framework',
+    'songs',
 ]
+
+REST_FRAMEWORK = {
+    'COERCE_DECIMAL_TO_STRING': False
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -110,3 +116,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from music_library_project.local_settings import *
+except ImportError:
+    pass
